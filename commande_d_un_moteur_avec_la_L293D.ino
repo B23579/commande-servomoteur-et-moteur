0e4767298a7c60f278433087cb64a2c766d7d291
B23579 <58120702+B23579@users.noreply.github.com>
2020-02-17 09:12:33 +0100

    Add files via upload

---
+++ commande_d_un_moteur_avec_la_L293D.ino
@@ -0,0 +1,27 @@
+int sens1=5;
+int sens2=6;
+void setup() {
+  pinMode(sens1,OUTPUT);
+  pinMode(sens2,OUTPUT);
+  digitalWrite(sens1,LOW);
+  digitalWrite(sens2,LOW);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(sens1,HIGH);
+  digitalWrite(sens2,LOW);
+  delay(100);
+   digitalWrite(sens1,LOW);
+  digitalWrite(sens2,LOW);
+  delay(1000);
+   digitalWrite(sens1,LOW);
+  digitalWrite(sens2,HIGH);
+  delay(1000); 
+  digitalWrite(sens1,LOW);
+  digitalWrite(sens2,LOW);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
